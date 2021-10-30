aac38ac47534afaa0e77812b277d93eaf14364d9
= <=>
2021-10-30 02:33:06 -0400

    updated styling imports, added dark mode mixin and color variables

---
+++ front/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "styles/index.module.scss";
+
+const Home: NextPage = () => {
+  return <h1 className={styles.text}>hello</h1>;
+};
+
+export default Home;
