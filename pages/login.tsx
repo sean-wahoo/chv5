6444c7d9224a92b3b16bf4d5d2e80cf58ffe494e
= <=>
2021-11-01 07:46:18 -0400

    temporarily updated login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import type { NextPage } from "next";
+=======
+import type { GetServerSideProps, NextPage } from "next";
+>>>>>>> 119a7a6 (temporarily updated login page)
 import { useState } from "react";
 import styles from "styles/login.module.scss";
 import {
@@ -7,6 +11,7 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
+<<<<<<< HEAD
 import GoogleButton from "react-google-button";
 import axios from "axios";
 
@@ -68,6 +73,43 @@ const Login: NextPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+=======
+
+const googleSignIn = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const user = result.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+};
+
+const emailSignIn = (email: string, password: string) => {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      console.table(error);
+    });
+};
+
+const Login: NextPage = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  console.log(JSON.stringify(props));
+>>>>>>> 119a7a6 (temporarily updated login page)
   return (
     <main className={styles.main}>
       <div className={styles.login_prompt}>
@@ -117,6 +159,7 @@ const Login: NextPage = () => {
             value={confirmPassword}
             placeholder="Super Secret Password Again"
           />
+<<<<<<< HEAD
           {/* /** you might be mixing some shit up so take a nap and figure this out
           later. right now you are going pretty smoothly, just forgetting
           whetehr or not you're working on registration or logging in and which
@@ -134,13 +177,24 @@ const Login: NextPage = () => {
                   { type: "email", display_name, image_path, email, password }
                 );
               }
+=======
+          <button
+            className={styles.email_button}
+            onClick={(e) => {
+              e.preventDefault();
+              emailSignIn(email, password);
+>>>>>>> 119a7a6 (temporarily updated login page)
             }}
           >
             Sign Up
           </button>
         </form>
+<<<<<<< HEAD
         <hr className={styles.line} />
         <GoogleButton onClick={() => googleSignIn()} />
+=======
+        <button onClick={() => googleSignIn()}>google</button>
+>>>>>>> 119a7a6 (temporarily updated login page)
       </div>
     </main>
   );
