145f2e97a9c4a47eb951493d48a8889115ef3071
= <=>
2021-11-04 03:43:55 -0400

    removed middleware

--- frontend/pages/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-export function middleware(req: NextRequest) {
-  //   return NextResponse.redirect("/");
-}
